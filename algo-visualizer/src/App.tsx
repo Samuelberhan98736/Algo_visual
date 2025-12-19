3f7ac43a02c26fd5a3232496bff41e583407c017
Samuel Berhan <berhansami10@gmail.com>
2025-12-18 20:51:25 -0500

    initial commit

---
+++ algo-visualizer/src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Algorithm Visualizer</h1>
+      <p>React + TypeScript + Vite</p>
+    </div>
+  );
+};
+
+export default App;
